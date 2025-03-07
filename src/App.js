770c39e3a40e0dac9a5f1ad8b1e744859ae39c4f
LHGiang <lehoaigiangg@gmail.com>
2025-03-07 20:56:21 +0700

    push

--- src/App.js
+++ src/App.js
@@ -83,7 +83,7 @@ const Title = styled(motion.h1)`
   transition={{ duration: 1 }}
   className="responsive-title"
 >
- Chúc mừng ngày 8/3, Ngọc Thịnh "Khờ" "ôn"!
+ Chúc mừng ngày 8/3, Ngọc Thịnh "Khờ" ......"ôn Khôn...."!
 </Title>
 
 const PuppyContainer = styled(motion.div)`
